e8b3c14cd7e0215552e92fe36f132bd99dc1c617
Mauri Demasi <demasimauri@gmail.com>
2022-11-01 09:52:19 -0300

    Envio de Mails

--- ProyectoFinal/Controllers/HomeController.cs
+++ ProyectoFinal/Controllers/HomeController.cs
@@ -23,6 +23,19 @@ namespace ProyectoFinal.Controllers
             return View(posts);
         }
 
+        public IActionResult Post(int id)
+        {
+            var rule = new PublicacionRule(_configuration);
+            var post = rule.GetPostById(id);
+            if (post== null)
+            {
+                return View("404");
+            }
+
+            return View(post);
+        }
+
+
         public IActionResult Suerte()
         {
             var rule = new PublicacionRule(_configuration);
@@ -39,6 +52,27 @@ namespace ProyectoFinal.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Contacto(Contacto contacto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Contacto", contacto);
+            }
+
+            var rule = new ContactoRule(_configuration);
+            var mensaje = @"<h1>Gracias por el contacto </h1>
+                           <p>Hemos recibido tu correo exitosamente. </p> 
+                           <p>Nos pondremos en contacto a la brevedad </p> 
+                            <hr><p>Saludos</p><p><b>Polo MC</b></p>  ";
+            rule.SendEmail(contacto.Email, mensaje, "Mensaje Recibido", "Polo Mincha Claverto", "polo@poloMc.com.ar");
+            rule.SendEmail("mauridemasi88@gmail.com",contacto.Mensaje, "Nuevo Contacto", contacto.Nombre, contacto.Email);
+
+
+            return View("Contacto");
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- ProyectoFinal/Rules/PublicacionRule.cs
+++ ProyectoFinal/Rules/PublicacionRule.cs
@@ -20,7 +20,7 @@ namespace ProyectoFinal.Rules
             using var connection = new SqlConnection(connectionString);
             {
                 connection.Open();
-                var posts = connection.Query<Publicacion>("SELECT TOP 1 * FROM Publicacion");
+                var posts = connection.Query<Publicacion>("SELECT TOP 1 * FROM Publicacion ORDER BY NEWID()");
 
                 return posts.First();
             }
@@ -37,6 +37,30 @@ namespace ProyectoFinal.Rules
                 connection.Open();
                 var posts = connection.Query<Publicacion>("SELECT TOP 4 * FROM Publicacion ORDER BY Creacion DESC");
                 return posts.ToList();
+            }
+
+
+        }
+
+
+        public Publicacion GetPostById(int id)
+        {
+            var connectionString = _configuration.GetConnectionString("BlogDatabase");
+
+            using var connection = new SqlConnection(connectionString);
+            {
+                connection.Open();
+
+                var query = "SELECT * FROM Publicacion WHERE Id=@id";
+                var posts = connection.QueryFirstOrDefault<Publicacion>(query, new {id});
+
+
+                //if (posts==null)
+                //{
+                //    return HttpNotFound();
+                //}
+
+                return posts;
             }
 
 
